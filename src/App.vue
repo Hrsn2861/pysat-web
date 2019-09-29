53872c422732d65c9ecea5ca88c859a41031ce0c
顾掀宇 <2017011401@secoder.net>
2019-09-29 15:06:50 +0800

    feat: create basic mainpage and set routing strategy

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,12 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: url("./assets/background16-9-2.jpg");
+  background-repeat: space;
+  background-blend-mode: saturation;
+  background-size: cover;
+  margin: 0%;
+  height: 100%;
+  width: 100%;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Index from '@/components/Index'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,21 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/index',
+      name: 'Index',
+      component: Index
     }
+    // {
+    //   path: '/signup',
+    //   name: 'Signup',
+    //   component: Signup
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'Login',
+    //   component: Login
+    // }
   ]
 })
