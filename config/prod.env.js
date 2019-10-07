6537d4ba5f2facf0e442d14f55efcb4e4081170e
jdyuhan <949917118@qq,com>
2019-10-07 22:56:51 +0800

    idiot: change from https to http

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-	VUE_APP_BASE_API:'"https://pysat-server-CTRL.app.secoder.net:80"'
+	VUE_APP_BASE_API:'"http://pysat-server-CTRL.app.secoder.net:80"'
 }
