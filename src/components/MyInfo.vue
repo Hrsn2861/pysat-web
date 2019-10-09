773d6c8c9a156b91a295541e081ecf83ece66185
qingyuqi <qyq17@mails.tsinghua.edu.cn>
2019-10-09 11:35:59 +0800

    feat: 1 #add user info page

--- src/components/MyInfo.vue
+++ src/components/MyInfo.vue
@@ -3,7 +3,10 @@
   <el-card class="box-card">
     <el-row type="flex" justify="center">
       <el-col :span="12">
-        <h1>手机号</h1>
+        <h1>个人信息</h1>
+        <p>UserName: {{ username }}</p>
+        <p>Phone: {{ phonenumber }}</p>
+        <p>Email: {{ email }}</p>
       </el-col>
     </el-row>
   </el-card>
@@ -11,6 +14,37 @@
 </template>
 
 <script>
+
+export default {
+  name: 'MyInfo',
+  data () {
+    return {
+      username: 'None',
+      phonenumber: 'None',
+      email: 'None'
+    }
+  },
+  watch: {
+    $route (to, from) {
+      console.log(to.path)
+      if (to.path === '/MyInfo') { console.log('个人信息') }
+    }
+  },
+  methods: {
+    getmyinfo (entrykey) {
+      this.$axios.get('/api/info', {param: entrykey}).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+        this.$message({
+          type: 'error',
+          message: 'Could not find',
+          duration: 1000
+        })
+      })
+    }
+  }
+}
 </script>
 
 <style>
