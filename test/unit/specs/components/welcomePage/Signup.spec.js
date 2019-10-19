c86dd16719a0bc7adb1130e0b7d95c3f99da9001
qingyuqi <qyq17@mails.tsinghua.edu.cn>
2019-10-19 22:04:55 +0800

    unit-test: separate Signup's test into pieces

--- test/unit/specs/components/welcomePage/Signup.spec.js
+++ test/unit/specs/components/welcomePage/Signup.spec.js
@@ -5,11 +5,14 @@ import { mount, createLocalVue } from 'vue-test-utils'
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-describe('testing signup.vue', () => {
+describe('注册页面测试', () => {
   let actions
   let state
   let store
-  beforeEach(() => {
+  let mockFn
+  let wrapper
+  let checkAfterClickButton
+  beforeAll(() => {
     state = {
       login: false
     }
@@ -20,10 +23,8 @@ describe('testing signup.vue', () => {
       state,
       actions
     })
-  })
-  const mockFn = jest.fn()
-  it('注册账号', () => {
-    const wrapper = mount(Signup, {
+    mockFn = jest.fn()
+    wrapper = mount(Signup, {
       store,
       localVue
     })
@@ -31,22 +32,28 @@ describe('testing signup.vue', () => {
       getCaptcha: mockFn,
       submitForm: mockFn
     })
-    let checkAfterClickButton = (expectedStep) => {
+    checkAfterClickButton = (expectedStep) => {
       wrapper.find('el-button').trigger('click')
       expect(wrapper.vm.step).toEqual(expectedStep)
     }
-    wrapper.vm.step = 1
+  })
+  it('初始化', () => {
+    expect(wrapper.vm.step).toEqual(1)
+  })
+  it('用户名格式检测', () => {
     // 用户名不能为空
     wrapper.vm.registerForm.userName = ''
     checkAfterClickButton(1)
-    // 用户名长度为3-10
+    // 用户名长度在3-10
     wrapper.vm.registerForm.userName = 'AB'
     checkAfterClickButton(1)
     wrapper.vm.registerForm.userName = 'ABCDEFGHIJKL'
     checkAfterClickButton(1)
-    // 第一步完成
+    // 格式正确
     wrapper.vm.registerForm.userName = 'ABCD'
     checkAfterClickButton(2)
+  })
+  it('密码格式检测', () => {
     // 密码不能为空
     wrapper.vm.registerForm.pwd = ''
     checkAfterClickButton(2)
@@ -59,28 +66,24 @@ describe('testing signup.vue', () => {
     checkAfterClickButton(2)
     wrapper.vm.registerForm.pwd = '$$$$$$$'
     checkAfterClickButton(2)
+    // 密码格式正确
     wrapper.vm.registerForm.pwd = 'ABcd123'
     checkAfterClickButton(3)
-    // 确认密码
+  })
+  it('确认密码', () => {
     wrapper.vm.registerForm.checkPwd = ''
     checkAfterClickButton(3)
     wrapper.vm.registerForm.checkPwd = 'Abcd123'
     checkAfterClickButton(3)
     wrapper.vm.registerForm.checkPwd = 'ABcd123'
     checkAfterClickButton(4)
-    // 手机号输入正确
+  })
+  it('手机号输入正确', () => {
     wrapper.vm.registerForm.phonenumber = ''
     checkAfterClickButton(4)
     wrapper.vm.registerForm.phonenumber = '12306'
     checkAfterClickButton(4)
     wrapper.vm.registerForm.phonenumber = '13306123062'
     checkAfterClickButton(5)
-    // 验证码
-    wrapper.find('el-button').trigger('click')
-    expect(mockFn).toBeCalled()
-    wrapper.vm.step++
-    // 提交
-    wrapper.find('el-button').trigger('click')
-    expect(mockFn).toHaveBeenCalledTimes(2)
   })
 })
