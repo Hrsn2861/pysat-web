7c468ede4079ff6e1be0b64dbd68eff228dc1f6a
禹含 <2016010609@secoder.net>
2019-10-23 13:07:33 +0800

    feat: continue to build judgetable

--- src/components/admin/JudgeTable.vue
+++ src/components/admin/JudgeTable.vue
@@ -7,10 +7,26 @@
     <el-table-column prop="submit_time" label="提交时间" width="180"></el-table-column>
     <el-table-column prop="author" label="作者" width="180"></el-table-column>
     <el-table-column prop="name" label="程序名" :formatter="formatter" width="200"></el-table-column>
+    <el-table-column prop="status" label="审核状态"  width="200"></el-table-column>
 
-    <el-table-column label="操作" width="150" fixed="right">
+    <el-table-column label="下载" width="150" fixed="right">
       <template slot-scope="scope">
-        <el-button type="text" @click="Operation(scope.row)">{{buttonText}}</el-button>
+        <el-button type="text" @click="Download(scope.row)">下载</el-button>
+      </template>
+    </el-table-column>
+        <el-table-column label="通过" width="150" fixed="right">
+      <template slot-scope="scope">
+        <el-button type="text" @click="ApproveOrNot(scope.row, true)">通过</el-button>
+      </template>
+    </el-table-column>
+        <el-table-column label="不通过" width="150" fixed="right">
+    <template slot-scope="scope">
+        <el-button type="text" @click="ApproveOrNot(scope.row, false)">不通过</el-button>
+      </template>
+    </el-table-column>
+    <el-table-column label="上传" width="150" fixed="right">
+      <template slot-scope="scope">
+        <el-button type="text" @click="Upload(scope.row)">上传</el-button>
       </template>
     </el-table-column>
   </el-table>
@@ -48,6 +64,7 @@ export default {
         res => {
           if (res.data.status === 1) {
             console.log(res.data.data)
+            // 需要更新row.status
           } else {
             this.$message.error(`${res.data.msg}`)
           }
@@ -56,6 +73,27 @@ export default {
           this.$message.error(`${err.message}`, 'ERROR!')
         }
       )
+    },
+    ApproveOrNot (row, approve) {
+      if (approve) {
+        console.log('Approve!')
+        console.log(row.name)
+      } else {
+        console.log('Disapprove!')
+        console.log(row.name)
+      }
+
+      // to be implemented
+      // 首先应当下载过（根据row.status判断），否则报错
+      // 如果通过，则仍然出现在列表里
+      // 如果不通过，则需要删掉
+    },
+    Upload (row) {
+      console.log('Upload')
+      console.log(row.name)
+      // to be implemented
+      // 首先应当已经审核通过，否则报错
+      // 上传成功后，需要从列表里面删掉
     }
   }
 }
