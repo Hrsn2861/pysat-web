d8599bb4ff02bd00c559d3e148fb5f80bac60c11
禹含 <2016010609@secoder.net>
2019-10-23 16:00:04 +0800

    feat: add @click approveornot

--- src/components/admin/JudgeTable.vue
+++ src/components/admin/JudgeTable.vue
@@ -11,22 +11,22 @@
 
     <el-table-column label="下载" width="150" fixed="right">
       <template slot-scope="scope">
-        <el-button type="text" @click="Download(scope.row)">下载</el-button>
+        <el-button type="text" @click="Download(scope.$index, scope.row)">下载</el-button>
       </template>
     </el-table-column>
         <el-table-column label="通过" width="150" fixed="right">
       <template slot-scope="scope">
-        <el-button type="text" @click="ApproveOrNot(scope.row, true)">通过</el-button>
+        <el-button type="text" @click="ApproveOrNot(scope.$index, scope.row, true)">通过</el-button>
       </template>
     </el-table-column>
         <el-table-column label="不通过" width="150" fixed="right">
     <template slot-scope="scope">
-        <el-button type="text" @click="ApproveOrNot(scope.row, false)">不通过</el-button>
+        <el-button type="text" @click="ApproveOrNot(scope.$index, scope.row, false)">不通过</el-button>
       </template>
     </el-table-column>
     <el-table-column label="上传" width="150" fixed="right">
       <template slot-scope="scope">
-        <el-button type="text" @click="Upload(scope.row)">上传</el-button>
+        <el-button type="text" @click="Upload(scope.$index, scope.row)">上传</el-button>
       </template>
     </el-table-column>
   </el-table>
@@ -52,7 +52,7 @@ export default {
     formatter (row, column) {
       return row.name
     },
-    Download (row) {
+    Download (index, row) {
       let tmpdata = {
         token: this.$store.getters.getUserToken,
         codeid: row.id
@@ -65,6 +65,7 @@ export default {
           if (res.data.status === 1) {
             console.log(res.data.data)
             // 需要更新row.status
+            row.status = 1
           } else {
             this.$message.error(`${res.data.msg}`)
           }
@@ -74,21 +75,61 @@ export default {
         }
       )
     },
-    ApproveOrNot (row, approve) {
-      if (approve) {
-        console.log('Approve!')
-        console.log(row.name)
-      } else {
-        console.log('Disapprove!')
-        console.log(row.name)
-      }
-
-      // to be implemented
+    ApproveOrNot (index, row, approve) {
       // 首先应当下载过（根据row.status判断），否则报错
       // 如果通过，则仍然出现在列表里
       // 如果不通过，则需要删掉
+      if (row.status === 0) {
+        this.$message.error('您应当先下载程序再进行审核！')
+        return
+      }
+      if (row.status === 2) {
+        this.$message.error('您已经审核通过了！')
+        return
+      }
+
+      let tmpdata = {
+        token: this.$store.getters.getUserToken,
+        codeid: row.id
+      }
+      if (approve) {
+        console.log('Approve!')
+        console.log(row.name)
+        tmpdata.judge = 1
+      } else {
+        console.log('Disapprove!')
+        console.log(row.name)
+        tmpdata.judge = 0
+      }
+      console.log(tmpdata)
+      myGet(
+        '/api/program/admin/judge',
+        tmpdata,
+        res => {
+          if (res.data.status === 1) {
+            console.log(res.data.data)
+            // 需要更新row.status
+            if (approve) {
+              row.status = 2
+            } else {
+              row.status = -1
+              this.tableData.splice(index, 1)
+            }
+          } else {
+            this.$message.error(`${res.data.msg}`)
+          }
+        },
+        err => {
+          this.$message.error(`${err.message}`, 'ERROR!')
+        }
+      )
     },
-    Upload (row) {
+    Upload (index, row) {
+      if (row.status < 2) {
+        this.$message.error('您应当先下载并审核通过该程序！')
+        return
+      }
+
       console.log('Upload')
       console.log(row.name)
       // to be implemented
