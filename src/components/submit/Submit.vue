7910de17dd50f89254e2df99814d27506a7d08ee
禹含 <2016010609@secoder.net>
2019-11-08 17:27:29 +0800

    refactor: add gettheminfo

--- src/components/submit/Submit.vue
+++ src/components/submit/Submit.vue
@@ -1,28 +1,7 @@
 <template>
   <div class="main-div">
     <el-card class="box-card">
-      <el-form>
-        <el-form-item label="版块">
-          <el-select v-model="moduleId" placeholder="版块" @change="GetThemeList">
-            <el-option
-              v-for="item in moduleList"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="主题">
-          <el-select v-model="currentThemeId" placeholder="主题">
-            <el-option
-              v-for="item in themeList"
-              :key="item.id"
-              :label="item.title"
-              :value="item.id"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
+      <el-tag type="info">{{themeInfo.title}}</el-tag>
       <AceContainer ref="AceContainer"></AceContainer>
       <el-col :span="4">
         <el-input
@@ -64,84 +43,31 @@ import { checkSession } from '@/utils/sessionUtils/sessionFunc'
 import { myPost } from '@/utils/requestFunc.js'
 
 import AceContainer from '@/components/submit/AceContainer.vue'
-import getSchoolAndThemeMixin from '@/utils/functionUtils/getThemeAndSchoolListMixin'
+import getThemeInfoMixin from '@/utils/functionUtils/getThemeInfoMixin'
 
 export default {
-  mixins: [getSchoolAndThemeMixin],
+  mixins: [getThemeInfoMixin],
+
   components: {
     AceContainer
   },
   beforeCreate () {
     checkSession(this, '', '/')
   },
-  created () {
-    this.permission_public = localStorage.getItem('permission_public')
-    this.permission_private = localStorage.getItem('permission_private')
-  },
+
   mounted () {
-    this.GetThemeList()
+    this.currentThemeId = this.$route.params.themeid
+    this.GetThemeInfo()
   },
   data () {
     return {
       codename: '',
       readme: '',
-
       currentThemeId: -1,
-      themeList: [
-        {
-          id: 0,
-          title: '共产主义'
-        },
-        {
-          id: 1,
-          title: '大家随意提交的题目'
-        }
-      ],
-      moduleId: 0,
-      moduleList: [
-        {
-          value: 0,
-          label: '在野',
-          disabled: this.permission_public < 1
-        },
-        {
-          value: 1,
-          label: '校内',
-          disabled: this.permission_private < 1
-        }
-      ],
-
-      fileList: [],
-      permission_public: -1,
-      permission_private: -1
+      fileList: []
     }
   },
   methods: {
-    GetThemeList () {
-      let tmpData = {
-        token: this.$store.getters.getUserToken,
-        school_id: -1
-      }
-      if (this.moduleId === 0) {
-        tmpData.school_id = 0
-      } else {
-        tmpData.school_id = localStorage.getItem('school_id')
-      }
-      console.log(tmpData)
-      this.GetThemeListFromMixin(tmpData).then(res => {
-        if (this.themeList.length === 0) { // 无主题
-          this.themeList.push(
-            {
-              id: -1,
-              title: '无主题',
-              disabled: true
-            }
-          )
-        }
-        this.currentThemeId = this.themeList[0].id
-      })
-    },
-
     readFile (file) {
       var reader = new FileReader()
       console.log(file.raw)
